c4d1a397b3443adf0cc23645b69eda9a2e10b798
Yock <chrisyockey88@gmail.com>
2022-09-07 15:57:36 -0600

    still fixing parser

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -19,47 +19,56 @@ namespace Y {
         /// Functions ///
 
         std::tuple<double , double , double , double> parseEqnLine(std::string line) {
-            std::cout << "[EQN] " << line << std::endl;
-            int aI , bI , cI , dI;
+            int i1 , i2 , i3;
             double a , b , c , d;
             bool found1 = false , found2 = false , found3 = false;
             std::string term1 , term2 , term3 , term4;
+
             for (int i = 0; i < line.length(); i++) {
-                std::cout << "[i] = " << line[i] << std::endl;
-                // if (line.substr(line[i],3) == "x^2") found1 = true;
-                if (line[i] == 'x') found2 = true;
-                if (line[i] == '=')  found3 = true;
+                if (line[i] == '^') { found1 = true; i1 = i; }
+                if (line[i] == 'x') { found2 = true; i2 = i; }
+                if (line[i] == '=') { found3 = true; i3 = i; }
             }
+
             if (!found1 && !found2 && !found3) {
                 std::cout << "{!!!} | [EQN -> " << line << "] is invalid" << std::endl;
+                std::cout << "[USAGE]: ./build/math <ax^2 + bx + c = d>" << std::endl;
                 exit(1);
             }
-            // term1 = line.substr()
+
+            a = std::stod(line.substr(0 , i1 - 1));
+            b = std::stod(line.substr(i1 + 3, i2 - i1 - 3));
+            c = std::stod(line.substr(i2 + 2 , i3 - 1));
+            d = std::stod(line.substr(i3 + 1 , line.length() - i3 - 1));
+            
             return std::make_tuple(a , b , c , d);
         }
 
         std::tuple<double , double> solveQuadratic (double a , double b , double c , double d) {
             double neg , pos , rt;
             double x;
+
             if ((pow(b,2) - 4 * a * c)< 0) {
                 std::cout << "Roots are complex" << std::endl;
-                return std::make_tuple(0.0f,0.0f);
+                return std::make_tuple(0.0f , 0.0f);
             } else if (a == 0 && b > 0) {
                 d -= c;
                 x = (d + 0.0f)/ (b + 0.0f);
-                return std::make_tuple(x + 0.0f,0.0f);
+                return std::make_tuple(x + 0.0f , 0.0f);
             } else if (a > 0 && b == 0) {
                 d -= c;
                 x = sqrt(d / b);
-                return std::make_tuple(x + 0.0f,0.0f);
+                return std::make_tuple(x + 0.0f , 0.0f);
             } else if (a == 0 && b == 0) {
                 std::cout << "Coeffiecients of x are 0\n[No Solutions]" << std::endl;
-                return std::make_tuple(0.0f,0.0f);
+                return std::make_tuple(0.0f , 0.0f);
             }
+
             c -= d;
             rt = sqrt((pow(b,2) - 4 * a * c));
             neg = (-b - rt) / (2 * a);
             pos = (-b + rt) / (2 * a);
+
             return std::make_tuple(pos , neg);
         }
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,14 @@ int main(int argc,char* argv[]) {
         std::cout << "{!!!} | [USAGE]: ./build/math <ax^2 + bx + c = d>" << std::endl;
         return 1;
     }
-
     std::string eqn;
     for (int i = 1; i < argc; i++) {
         eqn += argv[i];
     }
     auto [a , b , c , d] = Y::math::parseEqnLine(eqn);
-    // auto [ posRt , negRt ] = Y::math::solveQuadratic(a , b , c , d);
-
-    // std::cout << "[EQN] x^2 - 2x - 120 = 0" << std::endl;
-    // std::cout << "Roots = {" << posRt << " , " << negRt << "}" << std::endl;
-    // std::cout << "Should be {12 , -10}" << std::endl << std::endl;
-
+    auto [ posRt , negRt ] = Y::math::solveQuadratic(a , b , c , d);
+    std::cout << "[EQN] " << eqn << std::endl;
+    std::cout << "Roots = {" << posRt << " , " << negRt << "}" << std::endl;
     return 0;
 }
 
